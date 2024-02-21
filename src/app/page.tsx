7896920eb28f0667159f446feaf8b9a6edccd906
Mauro <mauromaio.dev@gmail.com>
2024-02-20 21:28:31 -0300

    feat: add gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 export default function Home() {
   return (
     <>
-      <span className="font-bold text-4xl">E aí dev! Tudo beleza?</span>
-      <div className="w-full h-12 rounded-lg">
-        Aqui é onde você encontrará componentes especialmente projetados para
-        copiar e colar em seus aplicativos. Acessível. Customizável. Código
-        aberto.
+      <div className="gap-8 flex flex-col">
+        <span className="font-bold text-4xl">E aí dev! Tudo beleza?</span>
+        <div className="w-full h-12 rounded-lg">
+          Aqui é onde você encontrará componentes especialmente projetados para
+          copiar e colar em seus aplicativos. Acessível. Customizável. Código
+          aberto.
+        </div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
       </div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
     </>
   );
 }
