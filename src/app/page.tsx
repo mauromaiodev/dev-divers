6f667ab7c7b248121ede31947fcdb2113f31ead3
Mauro <mauromaio.dev@gmail.com>
2024-02-24 11:16:47 -0300

    feat: syntax adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@ export default function Home() {
   return (
     <>
       <div className="gap-8 flex flex-col">
-        <span className="font-bold text-4xl">E aí dev! Tudo beleza?</span>
+        <span className="font-bold text-4xl">E aí,dev! Tudo beleza?</span>
         <div className="w-full h-12 rounded-lg">
-          Aqui é onde você encontrará componentes especialmente projetados para
-          copiar e colar em seus aplicativos. Acessível. Customizável. Código
-          aberto.
+          Aqui você encontrará componentes especialmente projetados para copiar
+          e colar em seus aplicativos. Acessível. Customizável. Código aberto.
         </div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+        <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
         <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
         <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
       </div>
