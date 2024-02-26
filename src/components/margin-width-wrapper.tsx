e3d52b5d64758bc088994aceceae8eaa48d78a64
Mauro <mauromaio.dev@gmail.com>
2024-02-25 23:07:40 -0300

    feat: add open context to nav components

--- src/components/margin-width-wrapper.tsx
+++ src/components/margin-width-wrapper.tsx
@@ -1,12 +1,18 @@
+'use client';
+
+import { useSideNav } from '@/contexts/SideNavContext';
 import { ReactNode } from 'react';
 
 export default function MarginWidthWrapper({
-  children,
+  children
 }: {
   children: ReactNode;
 }) {
+  const { open } = useSideNav();
   return (
-    <div className="flex flex-col md:ml-60 sm:border-r sm:border-zinc-700 min-h-screen">
+    <div
+      className={`flex flex-col ${open ? 'md:ml-60' : 'md:ml-16'} sm:border-r sm:border-zinc-700 min-h-screen duration-300`}
+    >
       {children}
     </div>
   );
--- src/components/side-nav.tsx
+++ src/components/side-nav.tsx
@@ -9,23 +9,43 @@ import { SIDENAV_ITEMS } from '@/constants';
 import { SideNavItem } from '@/types';
 import { Icon } from '@iconify/react';
 import DevIcon from './icons/dev-logo';
+import { useSideNav } from '@/contexts/SideNavContext';
 
 const SideNav = () => {
+  const { open, toggleCollapse } = useSideNav();
+
   return (
-    <div className="md:w-60 dark:border-neutral-700 h-screen flex-1 fixed border-r hidden md:flex">
+    <div
+      className={`${open ? 'md:w-60' : 'md:w-16'} dark:border-neutral-700 h-screen flex-1 fixed border-r hidden md:flex duration-300`}
+    >
       <div className="flex flex-col space-y-6 w-full">
         <Link
           href="/"
           className="flex flex-row space-x-3 items-center justify-center md:justify-start md:px-6 border-b h-12 w-full dark:border-neutral-700"
         >
           <DevIcon className="w-7 h-7" />
-          <span className="font-bold text-xl hidden md:flex">DevDivers</span>
+          {open && (
+            <span className={`font-bold text-xl hidden md:flex`}>
+              DevDivers
+            </span>
+          )}
         </Link>
 
-        <div className="flex flex-col space-y-2 md:px-6 ">
-          {SIDENAV_ITEMS.map((item, idx) => {
-            return <MenuItem key={idx} item={item} />;
+        <div
+          className={`flex flex-col space-y-2 ${open ? 'md:px-6' : 'md:px-3'}`}
+        >
+          {SIDENAV_ITEMS.map((item, index) => {
+            return <MenuItem key={index} item={item} />;
           })}
+          <button
+            onClick={toggleCollapse}
+            className={`flex absolute -right-2.5 -translate-y-1/2 bg-zinc-700 rounded-full hover-bg-hoverBackground hover:bg-hoverBackground 
+          }`}
+          >
+            <div className={`${open ? 'rotate-180' : ''}`}>
+              <Icon icon="lucide:chevron-right" width="24" height="24" />
+            </div>
+          </button>
         </div>
       </div>
     </div>
@@ -37,12 +57,13 @@ export default SideNav;
 const MenuItem = ({ item }: { item: SideNavItem }) => {
   const pathname = usePathname();
   const [subMenuOpen, setSubMenuOpen] = useState(false);
+  const { open } = useSideNav();
   const toggleSubMenu = () => {
     setSubMenuOpen(!subMenuOpen);
   };
 
   return (
-    <div className="">
+    <div className="flex flex-col w-full">
       {item.submenu ? (
         <>
           <button
@@ -53,7 +74,9 @@ const MenuItem = ({ item }: { item: SideNavItem }) => {
           >
             <div className="flex flex-row space-x-4 items-center">
               {item.icon}
-              <span className="font-semibold text-xl  flex">{item.title}</span>
+              {open && (
+                <span className="font-semibold text-xl flex">{item.title}</span>
+              )}
             </div>
 
             <div className={`${subMenuOpen ? 'rotate-180' : ''} flex`}>
@@ -63,16 +86,16 @@ const MenuItem = ({ item }: { item: SideNavItem }) => {
 
           {subMenuOpen && (
             <div className="my-2 ml-12 flex flex-col space-y-4">
-              {item.subMenuItems?.map((subItem, idx) => {
+              {item.subMenuItems?.map((subItem, index) => {
                 return (
                   <Link
-                    key={idx}
+                    key={index}
                     href={subItem.path}
                     className={`${
                       subItem.path === pathname ? 'font-bold' : ''
                     }`}
                   >
-                    <span>{subItem.title}</span>
+                    {open && <span>{subItem.title}</span>}
                   </Link>
                 );
               })}
@@ -87,7 +110,9 @@ const MenuItem = ({ item }: { item: SideNavItem }) => {
           }`}
         >
           {item.icon}
-          <span className="font-semibold text-xl flex">{item.title}</span>
+          {open && (
+            <span className="font-semibold text-xl flex">{item.title}</span>
+          )}
         </Link>
       )}
     </div>
